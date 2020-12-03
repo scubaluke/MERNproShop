72159fdd54ec8f11ef2c74c7ae95c6d6a80c4643
Luke Gartland <lukegartland@gmail.com>
2020-12-02 17:04:22 -1000

    products loaded with redux

--- backend/routs/productRoutes.js
+++ backend/routs/productRoutes.js
@@ -11,7 +11,7 @@ import Product from '../models/productModel.js'
 
 router.get('/', asyncHandler(async (req, res) => {
 const products = await Product.find({})
-
+    
     res.json(products)
 }))
 
