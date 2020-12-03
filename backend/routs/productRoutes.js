f3ae8f04bba66c4da0ed8a38b627fde048700339
Luke Gartland <lukegartland@gmail.com>
2020-12-02 14:03:51 -1000

    custom error handling

--- backend/routs/productRoutes.js
+++ backend/routs/productRoutes.js
@@ -24,7 +24,8 @@ const product = await Product.findById(req.params.id)
 if(product) {
     res.json(product)
 } else {
-    res.status(404).json({ message: 'Product not found' })
+    res.status(404)
+    throw new Error('product not found')
 }
 } ))
 
