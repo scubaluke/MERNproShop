7445a06520f67fe21e22510071b24d7f201fa416
Luke Gartland <lukegartland@gmail.com>
2021-03-21 12:08:35 -1000

    user auth

--- backend/routs/productRoutes.js
+++ backend/routs/productRoutes.js
@@ -1,32 +1,13 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
+import { getProductById, getProducts } from '../controllers/productController.js'
 
-import Product from '../models/productModel.js'
 
 
-// @desc fetch all products
-// @route Get /api/products 
-// @access  Public 
 
-router.get('/', asyncHandler(async (req, res) => {
-const products = await Product.find({})
-    
-    res.json(products)
-}))
+router.route('/').get(getProducts)
 
-// @desc fetch single products
-// @route Get /api/products/:id
-// @access  Public 
-router.get('/:id', asyncHandler(async(req, res) => {
-const product = await Product.findById(req.params.id)
+router.route('/:id').get(getProductById)
 
-if(product) {
-    res.json(product)
-} else {
-    res.status(404)
-    throw new Error('product not found')
-}
-} ))
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,23 @@ import connectDB from './config/db.js'
 import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routs/productRoutes.js'
+import userRoutes from './routs/userRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express() 
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('API is running...')
 } )
 
 app.use('/api/products', productRoutes)
+
+app.use('/api/users', userRoutes)
+
 
 app.use(notFound)
 
