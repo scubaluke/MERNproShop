a066e5b0a70b4342fc4967d0384b5ed39e39a39e
Luke Gartland <lukegartland@gmail.com>
2021-06-22 07:51:27 -0500

    api route to pay with paypal

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,13 +43,11 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access  private 
 
 const getOrderById = asyncHandler(async (req, res) => {
-    console.log('getorderById ran');
    const order = await Order.findById(req.params.id).populate(
     'user',
     'name email',
     User
   )
-  console.log(await order);
 
    if(order) {
        res.json(order)
@@ -59,7 +57,36 @@ const getOrderById = asyncHandler(async (req, res) => {
    }
  })
 
+
+
+// @desc update order to paid
+// @route Get /api/orders/:id/pay
+// @access  private 
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+ 
+    if(order) {
+    order.isPaid =true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.payer.email_address
+    }
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+  })
+ 
 export  {
     addOrderItems,
-    getOrderById
+    getOrderById,
+    updateOrderToPaid
 }
