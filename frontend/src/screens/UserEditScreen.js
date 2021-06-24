8f14dc918b41f670364da99787fb9dcc034ccf36
Luke Gartland <lukegartland@gmail.com>
2021-06-24 08:49:39 -0500

    update user in admin pannel

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/UserConstants'
 
 
 const UserEditScreen = ({ match, history }) => {
@@ -20,20 +21,27 @@ const UserEditScreen = ({ match, history }) => {
     const userDetails = useSelector(state => state.userDetails)
     const {loading, error, user} = userDetails
 
+    const userUpdate = useSelector(state => state.userUpdate)
+    const {loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate
 
     useEffect(() => {
-       if(!user.name || user._id !== userId) {
-           dispatch(getUserDetails(userId))
-       } else {
-           setName(user.name)
-           setEmail(user.email)
-           setIsAdmin(user.isAdmin)
-       }
-    }, [user, dispatch, userId])
+        if(successUpdate) {
+            dispatch({ type: USER_UPDATE_RESET })
+            history.push('/admin/userlist')
+        } else {
+            if(!user.name || user._id !== userId) {
+                dispatch(getUserDetails(userId))
+            } else {
+                setName(user.name)
+                setEmail(user.email)
+                setIsAdmin(user.isAdmin)
+            }
+        }
+    }, [user, dispatch, userId, successUpdate, history])
 
     const submitHandler = (e) => {
         e.preventDefault()
-     
+     dispatch(updateUser({ _id: userId, name, email, isAdmin }))
     }
 
     return (
@@ -43,6 +51,8 @@ const UserEditScreen = ({ match, history }) => {
             </Link>
             <FormContainer>
             <h1>Edit user</h1>
+            {loadingUpdate && <Loader /> }
+            {errorUpdate && <Message variant='danger' >{errorUpdate}</Message> }
            {loading ? <Loader /> : error ? <Message variant='danger' >{error}</Message> : (
                 <Form onSubmit={submitHandler} >
                 <Form.Group controlId='name'>
