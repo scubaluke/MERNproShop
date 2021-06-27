ef782c1514a9711946cc77faf3bd0e712276cb7d
Luke Gartland <lukegartland@gmail.com>
2021-06-27 12:17:29 -0500

    add product review functionality

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -14,7 +14,11 @@ import {
     PRODUCT_CREATE_SUCCESS,
     PRODUCT_UPDATE_FAIL,
     PRODUCT_UPDATE_SUCCESS,
-    PRODUCT_UPDATE_REQUEST
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_CREATE_REVIEW_FAIL,
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_RESET,
+    PRODUCT_CREATE_REVIEW_SUCCESS
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -141,6 +145,39 @@ export const updateProduct = (product) => async (dispatch, getState) => {
           : error.message
         dispatch({
             type: PRODUCT_UPDATE_FAIL,
+            payload: message
+        })
+    }
+} 
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_REQUEST,
+        })
+
+        const { userLogin: {userInfo} } = getState()
+
+        const config ={
+            headers: {
+                'Content_Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+       
+        await axios.post(`/api/products/${productId}/reviews`, review, config)
+
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_SUCCESS,
+        })
+
+    } catch (error) {
+        const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_FAIL,
             payload: message
         })
     }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,27 +6,46 @@ import Rating from '../components/Rating'
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-import { listProductDetails } from '../actions/productActions'
-
+import { listProductDetails, createProductReview } from '../actions/productActions'
+import { PRODUCT_CREATE_RESET, PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants'
 
 const ProductScreen = ({ history, match }) => {
     const [qty, setQty] = useState(1)
-    // const [rating, setRating] = useState(0)
-    // const [comment, setComment] = useState('')
-    console.log(qty);
+    const [rating, setRating] = useState(0)
+    const [comment, setComment] = useState('')
 
     const dispatch = useDispatch()
 
     const productDetails = useSelector(state => state.productDetails)
     const { loading, error, product } = productDetails
 
+    const productReviewCreate = useSelector(state => state.productReviewCreate)
+    const { error: errorProductReview, success: successProductReview } = productReviewCreate
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
     useEffect(() => {
+        if(successProductReview) {
+            alert('Review Submitted!')
+            setRating(0)
+            setComment('')
+            dispatch({ type: PRODUCT_CREATE_RESET })
+        }
             dispatch(listProductDetails(match.params.id))
-       }, [dispatch, match])
+       }, [dispatch, match, successProductReview])
 
 const addToCartHandler = () => {
     history.push(`/cart/${match.params.id}?qty=${qty}`)
 }
+
+    function submitHandler(e) {
+        e.preventDefault()
+        dispatch(createProductReview(match.params.id, {
+            rating,
+            comment
+        }))
+    }
 
        return (
         <>
@@ -34,7 +53,8 @@ const addToCartHandler = () => {
                Go Back
                </Link>
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-  <Row>
+ <>
+ <Row>
   <Col  md={6}>
 <Image src={product.image} alt={product.name} fluid ></Image>
   </Col>
@@ -107,6 +127,51 @@ const addToCartHandler = () => {
      </Card>
  </Col>
 </Row>
+<Row>
+    <Col md={6}>
+        <h2>Reviews</h2>
+        {product.reviews.length === 0 && <Message>No reviews</Message>}
+        <ListGroup variant='flush' >
+            {product.reviews.map(review => (
+                <ListGroup.Item key={review._id} >
+                    <strong>
+                        {review.name}
+                    </strong>
+                    <Rating value={review.rating} ></Rating>
+                    <p>{review.createdAt.substring(0,10)}</p>
+                    <p>{review.comment}</p>
+                </ListGroup.Item>
+            ))}
+            <ListGroup.Item>
+                <h2>Write a Customer Review</h2>
+                {errorProductReview && <Message variant="danger" >{errorProductReview}</Message>}
+                {userInfo ? (
+                    <Form onSubmit={submitHandler} >
+                        <Form.Group controlId='rating'>
+                            <Form.Label>Rating</Form.Label>
+                            <Form.Control as="select"  value={rating} onChange={(e) => setRating(e.target.value) } >
+                                <option value="">Select...</option>
+                                <option value="1">1 - Poor</option>
+                                <option value="2">3 - Fair</option>
+                                <option value="3">3 - Good</option>
+                                <option value="4">4 - Very Good</option>
+                                <option value="5">5 - Excellent</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId='comment' >
+                            <Form.Label>comment</Form.Label>
+                            <Form.Control as='textarea' row='3' value={comment} onChange={(e) => setComment(e.target.value)} ></Form.Control>
+                        </Form.Group>
+                        <Button type='submint' variant='primary' >
+                            Submit
+                        </Button>
+                    </Form>
+                ) : <Message>Please <Link to='/login' >log In</Link> to write a review</Message>}
+            </ListGroup.Item>
+        </ListGroup>
+    </Col>
+</Row>
+</>
             )}
          
         </>
