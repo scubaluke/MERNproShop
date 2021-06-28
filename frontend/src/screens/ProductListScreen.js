23f91236448de17e7091b1ef1f01659dc50c0d56
Luke Gartland <lukegartland@gmail.com>
2021-06-27 21:12:09 -0500

    paginate products on adminn screen

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -6,12 +6,15 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
+import Paginate from '../components/Paginate'
 
 export default function ProductListScreen({ history, match }) {
+    const pageNumber = match.params.pageNumber || 1
+
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList) 
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     const productDelete = useSelector(state => state.productDelete) 
     const {loading: loadingDelete,  error: errorDelete, success: successDelete } = productDelete
@@ -32,10 +35,10 @@ export default function ProductListScreen({ history, match }) {
         if(successCreate) {
             history.push(`/admin/product/${createdProduct._id}/edit`)
         } else {
-            dispatch(listProducts())
+            dispatch(listProducts('', pageNumber))
         }
       
-    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct, pageNumber])
 
     function deleteHandler(id) {
         if(window.confirm('Are you sure you want to permanently delete this user?')) {
@@ -63,6 +66,7 @@ return (
     {loadingCreate && <Loader />}
     {errorCreate && <Message variant='danger'>{errorCreate}</Message> }
       {loading ? <Loader /> : error  ? <Message variant='danger'>{error}</Message> : (
+          <>
           <Table striped  bordered hover responsive className='table-sm' >
               <thead>
                   <tr>
@@ -103,6 +107,8 @@ return (
                   ))}
               </tbody>
           </Table>
+          <Paginate pages={pages} page={page} isAdmin={true} ></Paginate>
+         </>
       ) }
       </>
     )
